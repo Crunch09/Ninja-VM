8bea4298acf625aeb107f7c5cd376eb26daae788
Florian Thomas <florianthomas90@gmail.com>
2011-04-05 00:04:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NINJA VM
+
+This will be a VM for Ninja (Ninja is not Java) programmed in C.
+
+Usage:
+
+`cd "DirectoryOfTheNinjaVM"`
+`gcc -o njvm njvm.c`
+`./njvm`
---
+++ njvm.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "njvm.h"
+
+int main(int argc, char *argv[]){
+	
+	
+	if(argc >= 2){
+		if(strcmp(argv[1], "--help") == 0){
+			printf("usage: ./njvm [option] [option] ... \n");
+			printf("      --version        show version and exit\n");
+			printf("      --help           show this help and exit\n");
+		}else if(strcmp(argv[1], "--version") == 0){
+			printf("Ninja Virtual Machine version 0 ( compiled %s, %s )\n", __DATE__, __TIME__);
+		}else{
+			printf("unknown command line argument '%s', try './njvm-linux --help' \n", argv[1]);
+		}
+	}else{
+		printf("Ninja Virtual Machine started\n");
+		printf("Ninja Virtual Machine stopped\n");
+	}
+	
+	
+	return 0;
+}
