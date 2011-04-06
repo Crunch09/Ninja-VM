d29b9ab23db8166d935b872485502c97b57b8c23
Florian Thomas <florianthomas90@gmail.com>
2011-04-06 08:22:03 +0200

    Added comments

--- njvm.c
+++ njvm.c
@@ -5,16 +5,20 @@ int main(int argc, char *argv[]){
 	
 	
 	if(argc >= 2){
+		/* Hilfe ausgeben */
 		if(strcmp(argv[1], "--help") == 0){
 			printf("usage: ./njvm [option] [option] ... \n");
 			printf("      --version        show version and exit\n");
 			printf("      --help           show this help and exit\n");
 		}else if(strcmp(argv[1], "--version") == 0){
+			/* Versionsinformationen ausgeben */
 			printf("Ninja Virtual Machine version 0 ( compiled %s, %s )\n", __DATE__, __TIME__);
 		}else{
+			/* Unbekannter Befehl */
 			printf("unknown command line argument '%s', try './njvm --help' \n", argv[1]);
 		}
 	}else{
+		/* Keine Parameter angegeben */
 		printf("Ninja Virtual Machine started\n");
 		printf("Ninja Virtual Machine stopped\n");
 	}
