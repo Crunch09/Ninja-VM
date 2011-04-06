f3ca7b4764a986643c98192d84ce78f99f378cd0
sutterh <hermann.sutter@mni.fh-giessen.de>
2011-04-06 08:44:41 +0200

    Funktion Hilfe Ausgabe

--- njvm.c
+++ njvm.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+void printHelp(void);
+
+
 int main(int argc, char *argv[]){
-	
-	
 	if(argc >= 2){
 		/* Hilfe ausgeben */
 		if(strcmp(argv[1], "--help") == 0){
-			printf("usage: ./njvm [option] [option] ... \n");
-			printf("      --version        show version and exit\n");
-			printf("      --help           show this help and exit\n");
+	    printHelp();
 		}else if(strcmp(argv[1], "--version") == 0){
 			/* Versionsinformationen ausgeben */
 			printf("Ninja Virtual Machine version 0 ( compiled %s, %s )\n", __DATE__, __TIME__);
@@ -25,4 +24,13 @@ int main(int argc, char *argv[]){
 	
 	
 	return 0;
+}
+
+/* Ausgabe von Option */
+void printHelp(void){
+  printf("./njvm [option] [option] ...\n");
+  printf("--program 1\n");
+  printf("--program 2\n");
+  printf("--help     print this page\n");
+  printf("--version  print version of programm\n");
 }
