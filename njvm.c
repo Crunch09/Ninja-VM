443eaa1a98e486be5036b710cf2b7b1655dc63b5
sutterh <hermann.sutter@mni.fh-giessen.de>
2011-04-06 08:59:30 +0200

    schleife fuer alle argumente eingefügt, variable fuer version eingefuegt

--- njvm.c
+++ njvm.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
+const char version[] = "0.1";
+
 void printHelp(void);
 
 
 int main(int argc, char *argv[]){
 	if(argc >= 2){
-		/* Hilfe ausgeben */
-		if(strcmp(argv[1], "--help") == 0){
-	    printHelp();
-		}else if(strcmp(argv[1], "--version") == 0){
-			/* Versionsinformationen ausgeben */
-			printf("Ninja Virtual Machine version 0 ( compiled %s, %s )\n", __DATE__, __TIME__);
-		}else{
-			/* Unbekannter Befehl */
-			printf("unknown command line argument '%s', try './njvm --help' \n", argv[1]);
-		}
+    int i;
+    for(i=1;i<argc;i++){
+		  /* Hilfe ausgeben */
+  		if(strcmp(argv[i],"--help")==0){
+	      printHelp();
+		  }else if(strcmp(argv[i],"--version")==0){
+			  /* Versionsinformationen ausgeben */
+			  printf("Ninja Virtual Machine version %s (compiled %s, %s)\n",version,__DATE__,__TIME__);
+		  }else if(strcmp(argv[i],"--program")==0){
+        if(strcmp(argv[i+1],"1")==0){}
+          /*program1();*/
+        else if(strcmp(argv[i+1],"2")==0){}
+          /*program2();*/
+	    }else{
+        /* Unbekannter Befehl */
+			  printf("unknown command line argument '%s', try './njvm --help' \n", argv[1]);
+		  }
+    }
 	}else{
 		/* Keine Parameter angegeben */
 		printf("Ninja Virtual Machine started\n");
