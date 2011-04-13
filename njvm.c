8cc193345465d4121fceae4f12c6fcee9b2579d1
Florian Thomas <florianthomas90@gmail.com>
2011-04-13 16:32:21 +0200

    Code wird jetzt aus .bin Datei gelesen.

--- njvm.c
+++ njvm.c
@@ -18,54 +18,60 @@
 
 #define stackSize 1024
 
-const char version[] = "0.1";
-/*const int stackSize = 1024;*/
+const char version[] = "0.2";
 int stack[stackSize];
 int stackPosition=0;
 int programCounter=0; /* zählt die Zeilen bei der Ausgabe */
-unsigned int code1[] = {(PUSHC << 24) | IMMEDIATE(3),
-                        (PUSHC << 24) | IMMEDIATE(4),
-                        (ADD << 24),
-                        (PUSHC << 24) | IMMEDIATE(10),
-                        (PUSHC << 24) | IMMEDIATE(6),
-                        (SUB << 24),(MUL << 24),
-                        (WRINT << 24),(HALT << 24)};
-
-unsigned int code2[] = {(PUSHC << 24) | IMMEDIATE(-2),
-	                (RDINT << 24),
-	                (MUL << 24),
-	                (PUSHC << 24) | IMMEDIATE(3),
-	                (ADD << 24),
-                        (WRINT << 24), (HALT << 24)};
+FILE *codeFile;
+int fileSize, numberOfCommands;
+unsigned int *programPointer;
 
 
 int main(int argc, char *argv[]){
   if(argc >= 2){
-    int i;
+    int i, fileClosed;
     for(i=1;i<argc;i++){
 		  /* Hilfe ausgeben */
-  	if(strcmp(argv[i],"--help")==0){
+  	  if(strcmp(argv[i],"--help")==0){
 	      printHelp();
-        }else if(strcmp(argv[i],"--version")==0){
+      }else if(strcmp(argv[i],"--version")==0){
 	      /* Versionsinformationen ausgeben */
 	      printf("Ninja Virtual Machine version %s (compiled %s, %s)\n",version,__DATE__,__TIME__);
-        }else if(strcmp(argv[i],"--program")==0){
-           if(strcmp(argv[i+1],"1")==0){
-        	program(code1,sizeof(code1)/sizeof(code1[0]));
-                break;
-           }
-           else if(strcmp(argv[i+1],"2")==0){
-        	program(code2,sizeof(code2)/sizeof(code2[0]));
-                break;
-           }
-	}else{
-        /* Unbekannter Befehl */
-	   printf("unknown command line argument '%s', try './njvm --help' \n", argv[i]);
-        }
-     }
-   }else{
+      
+	  }else{
+		codeFile = fopen(argv[i], "r");
+		if(codeFile == NULL || (strstr(argv[i], ".bin") == NULL )){
+			printf("Couldn't open %s . You need to use a .bin file. Try again. \n", argv[i]);
+		}else{
+			printf("Opened %s successful. \n", argv[i]);
+			/* Zeiger ans Ende setzen, um Größe zu ermitteln */
+			fseek(codeFile, 0, SEEK_END);
+			fileSize = ftell(codeFile);
+			/* Zeiger wieder an den Anfang setzen */
+			fseek(codeFile, 0, SEEK_SET);
+			programPointer = malloc(fileSize);
+			if( programPointer == NULL){
+				printf("No RAM available. \n");
+				exit(-99);
+			}
+			printf("Size: %d Bytes \n", fileSize);
+			numberOfCommands = fread(programPointer, 4, fileSize/4, codeFile);
+			if(numberOfCommands < fileSize/sizeof(programPointer[0])){
+				printf("There occured an error reading the file.  \n");
+				exit(-99);
+			}
+			program(programPointer, fileSize/sizeof(programPointer[0]));
+			fileClosed = fclose(codeFile);
+			if(fileClosed != 0){
+				printf("Warning ! Couldn't close the file. \n");
+			}
+		}
+       
+      }
+    }
+  }else{
 	   printf("No Arguments, try --help.\n");
-	}
+  }
 
    return 0;
 }
@@ -74,8 +80,7 @@ int main(int argc, char *argv[]){
 /* Ausgabe von Option */
 void printHelp(void){
   printf("./njvm [option] [option] ...\n");
-  printf("--program 1\n");
-  printf("--program 2\n");
+printf("./njvm path/to/filename.bin executes this code\n");
   printf("--help     print this page\n");
   printf("--version  print version of programm\n");
 }
@@ -127,58 +132,46 @@ void program(unsigned int *code,int size){
     instruction=(code[i]&0xFF000000)>>24;
 
     if(instruction==HALT){
-      /*printf("%03d: halt\n",programCounter);*/
       break;
     }else if(instruction==PUSHC){ /*schreiben in stack*/
-      /*printf("%03d: pushc  %d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));*/
       push(code[i]);
 
     }else if(instruction==ADD){
-      /*printf("%03d: add\n",programCounter);*/
       n1=pop();
       n2=pop();
       push(n2+n1);
 
     }else if(instruction==SUB){
-      /*printf("%03d: sub\n",programCounter);*/
       n1=pop();
       n2=pop();
       push(n2-n1);
 
     }else if(instruction==MUL){
-      /*printf("%03d: mul\n",programCounter);*/
       n1=pop();
       n2=pop();
       push(n2*n1);
 
     }else if(instruction==DIV){
-      /*printf("%03d: div\n",programCounter);*/
       n1=pop();
       n2=pop();
 
       if(n2!=0){
-        /*stack[stackPosition-2]=n1/n2;*/
         push(n2/n1);
       }
       /*stackPosition--;*/
     }else if(instruction==MOD){
-      /*printf("%03d: mod\n",programCounter);*/
       n1=pop();
       n2=pop();
       push(n1%n2);
 
     }else if(instruction==RDINT){
 	    /* liest Zahl auf der konsole ein */
-	    /*printf("%03d: rdint ",programCounter);*/
-	    scanf("%d", &eingeleseneZahl);
+	  scanf("%d", &eingeleseneZahl);
       push(eingeleseneZahl);
-	    /*stack[stackPosition] = eingeleseneZahl;
-	    stackPosition++;*/
     }else if(instruction==WRINT){
       printf("%d\n",SIGN_EXTEND(stack[stackPosition-1]&0x00FFFFFF));
     }
-    /* count für Zeilenausgabe erhöhen */
-	 /* programCounter++;*/
+
   }
 
   stackPosition=0;
