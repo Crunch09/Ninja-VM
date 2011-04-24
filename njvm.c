29ebdb8a397c44d857b3535ee3b1ab17bc9c75ea
sutterh <hermann.sutter@mni.fh-giessen.de>
2011-04-24 10:57:20 +0200

    debuger vollständig implementiert, bug noch nicht behoben

--- njvm.c
+++ njvm.c
@@ -37,57 +37,41 @@
 
 #define stackSize 1024
 
-const char version[] = "0.2";
+const char version[] = "0.3";
 int stack[stackSize];
 int stackPointer=0;
 int framePointer=0;
 int programCounter=0; /* zählt die Zeilen bei der Ausgabe */
 FILE *codeFile;
-int fileSize, numberOfCommands;
+int fileSize, fileClosed, numberOfCommands;
 unsigned int *programPointer;
 
 
 /* main */
 int main(int argc, char *argv[]){
   if(argc >= 2){
-    int i, fileClosed;
+    int i;/*, fileClosed;*/
     for(i=1;i<argc;i++){
       /* Hilfe ausgeben */
       if(strcmp(argv[i],"--help")==0){
         printHelp();
       }else if(strcmp(argv[i],"--version")==0){
         /* Versionsinformationen ausgeben */
-        printf("Ninja Virtual Machine version %s (compiled %s, %s)\n",version,__DATE__,__TIME__); 
+        printf("Ninja Virtual Machine version %s (compiled %s, %s)\n",version,__DATE__,__TIME__);
+      }else if(strcmp(argv[i],"--debug")==0){
+	openFile(i+1,argc,argv);
+	debug();
+	closeFile();
+	break;
       }else{
-        codeFile = fopen(argv[i], "r");
-        if(codeFile == NULL || (strstr(argv[i], ".bin") == NULL )){
-          printf("Couldn't open %s . You need to use a .bin file. Try again. \n", argv[i]);
-        }else{
-          printf("Opened %s successful. \n", argv[i]);
-          /* Zeiger ans Ende setzen, um Größe zu ermitteln */
-          fseek(codeFile, 0, SEEK_END);
-          fileSize = ftell(codeFile);
-          /* Zeiger wieder an den Anfang setzen */
-          fseek(codeFile, 0, SEEK_SET);
-          programPointer = malloc(fileSize);
-          if( programPointer == NULL){
-            printf("No RAM available. \n");
-            exit(-99);
-          }
-          printf("Size: %d Bytes \n", fileSize);
-          numberOfCommands = fread(programPointer, 4, fileSize/4, codeFile);
-          if(numberOfCommands < fileSize/sizeof(programPointer[0])){
-            printf("There occured an error reading the file.  \n");
-            exit(-99);
-          }
-          program(programPointer, fileSize/sizeof(programPointer[0]));
-          fileClosed = fclose(codeFile);
-          if(fileClosed != 0){
-            printf("Warning ! Couldn't close the file. \n");
-          }
-        }    
+	printf("Ninja Virtual Machine started\n");
+	openFile(i,argc,argv);
+	for(programCounter=0;programCounter<(fileSize/sizeof(programPointer[0]));programCounter++)
+	  program(programPointer);
+	closeFile();
+	printf("Ninja Virtual Machine stopped\n");
       }
-    }
+    }    
   }else{
     printf("No Arguments, try --help.\n");
   }
@@ -96,181 +80,254 @@ int main(int argc, char *argv[]){
 }
 
 
+/* open a bin-file */
+void openFile(int i,int argc, char *argv[]){
+  codeFile = fopen(argv[i], "r");
+  if(codeFile == NULL || (strstr(argv[i], ".bin") == NULL )){
+    printf("Couldn't open %s . You need to use a .bin file. Try again. \n", argv[i]);
+  }else{
+    printf("Opened %s successful. \n", argv[i]);
+    /* Zeiger ans Ende setzen, um Größe zu ermitteln */
+    fseek(codeFile, 0, SEEK_END);
+    fileSize = ftell(codeFile);
+    /* Zeiger wieder an den Anfang setzen */
+    fseek(codeFile, 0, SEEK_SET);
+    programPointer = malloc(fileSize);
+    if( programPointer == NULL){
+      printf("No RAM available. \n");
+      exit(-99);
+    }
+    printf("Size: %d Bytes \n\n", fileSize);
+    numberOfCommands = fread(programPointer, 4, fileSize/4, codeFile);
+    if(numberOfCommands < fileSize/sizeof(programPointer[0])){
+      printf("There occured an error reading the file.  \n");
+      exit(-99);
+    }
+  }    
+}
+/* close a file */
+void closeFile(void){
+  fileClosed = fclose(codeFile);
+  if(fileClosed != 0){
+    printf("Warning ! Couldn't close the file. \n");
+  }
+}
+
+
 /* Ausgabe von Option */
 void printHelp(void){
   printf("./njvm [option] [option] ...\n");
-  printf("./njvm path/to/filename.bin executes this code\n");
-  printf("--help     print this page\n");
-  printf("--version  print version of programm\n");
+  printf("./njvm path/to/filename.bin    executes this code\n");
+  printf("--help                         print this page\n");
+  printf("--version                      print version of programm\n");
+  printf("--debug path/to/filename.bin   debug mode for program\n\n");
+}
+
+
+/* debuger */
+void debug(void){
+  int numberInstructions,tempInstruction,j;
+  char inputString[5];
+
+  numberInstructions=fileSize/sizeof(programPointer[0]);
+  programCounter=0;
+
+  printf("Ninja Virtual Machine started\n");
+  printProgram(programPointer);
+
+  while(programCounter<numberInstructions){
+    printf("DEBUG: is (inspect stack), l (list), s (step), r (run), q (quit)?: ");
+    scanf("%s",inputString);
+
+    if(strcmp(inputString,"is")==0){
+      for(j=stackPointer;j>-1;j--){ /* stack von oben nach unten durchgehen */
+	if(j==stackPointer && j==framePointer)
+	  printf("sp, fp ---> %04d: xxxx\n",j);
+	else if(j==stackPointer)
+	  printf("sp -------> %04d: xxxx\n",j);
+	else if(j==framePointer)
+	  printf("fp -------> %04d: %4d\n",j,stack[j]);
+	else
+	  printf("            %04d: %4d\n",j,stack[j]);
+      }
+      printf("--- bottom of stack ---\n");
+      printProgram(programPointer);
+    }else if(strcmp(inputString,"l")==0){
+      tempInstruction=programCounter; /* temporaere speicherung programCounter  */
+      for(programCounter=0;programCounter<numberInstructions;programCounter++)
+	printProgram(programPointer);
+
+      printf("--- end of code ---\n");
+      programCounter=tempInstruction;
+      printProgram(programPointer);
+    }else if(strcmp(inputString,"s")==0){ /* naechster programcode wird ausgegeben und ausgefuehrt */
+      program(programPointer);
+      programCounter++;
+      printProgram(programPointer);
+    }else if(strcmp(inputString,"r")==0){ /* program laeuft normal ab */
+      for(;programCounter<numberInstructions;programCounter++)
+        program(programPointer);
+      break;
+    }else if(strcmp(inputString,"q")==0){ /* beenden des debugers */
+      break;
+    }else{
+      printf("Command unknown, try again\n");
+    }
+  }
+
+  printf("Ninja Virtual Machine stopped\n");
 }
 
 
 /* Gibt das Programm aus */
-void printProgram(unsigned int *code, int size){
-  int zeile, i;
+void printProgram(unsigned int *code){
+  int zeile;
   
-  for(i=0;i<size;i++){
-     
-    zeile = (code[i]&0xFF000000)>>24;
+  zeile = (code[programCounter]&0xFF000000)>>24;
   
-    if(zeile==HALT){
-      printf("%03d: halt\n",programCounter);
-    }else if(zeile==PUSHC){
-      printf("%03d: pushc %5d\n", programCounter, (SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==ADD){
-      printf("%03d: add\n",programCounter);
-    }else if(zeile==SUB){
-      printf("%03d: sub\n",programCounter);
-    }else if(zeile==MUL){
-      printf("%03d: mul\n",programCounter);
-    }else if(zeile==DIV){
-      printf("%03d: div\n",programCounter);
-    }else if(zeile==MOD){
-      printf("%03d: mod\n",programCounter);
-    }else if(zeile==RDINT){
-      printf("%03d: rdint\n",programCounter);
-    }else if(zeile==WRINT){
-      printf("%03d: wrint\n",programCounter);
-    }else if(zeile==ASF){
-      printf("%03d: asf %7d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==RSF){
-      printf("%03d: rsf\n",programCounter);
-    }else if(zeile==PUSHL){
-      printf("%03d: pushl %5d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==POPL){
-      printf("%03d: popl %6d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==EQ){
-      printf("%03d: eq \n",programCounter);
-    }else if(zeile==NE){
-      printf("%03d: ne \n",programCounter);
-    }else if(zeile==LT){
-      printf("%03d: lt \n",programCounter);
-    }else if(zeile==LE){
-      printf("%03d: le \n",programCounter);
-    }else if(zeile==GT){
-      printf("%03d: gt \n",programCounter);
-    }else if(zeile==GE){
-      printf("%03d: ge \n",programCounter);
-    }else if(zeile==JMP){
-      printf("%03d: jmp %2d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==BRF){
-      printf("%03d: brf %2d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==BRT){
-      printf("%03d: brt %2d\n",programCounter,(SIGN_EXTEND(code[i]&0x00FFFFFF)));
-    }else if(zeile==PO){
-      printf("%03d: po\n",programCounter);
-    }
-
-    programCounter++;
-    
+  if(zeile==HALT){
+    printf("%03d: halt\n",programCounter);
+  }else if(zeile==PUSHC){
+    printf("%03d: pushc %5d\n", programCounter, (SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==ADD){
+    printf("%03d: add\n",programCounter);
+  }else if(zeile==SUB){
+    printf("%03d: sub\n",programCounter);
+  }else if(zeile==MUL){
+    printf("%03d: mul\n",programCounter);
+  }else if(zeile==DIV){
+    printf("%03d: div\n",programCounter);
+  }else if(zeile==MOD){
+    printf("%03d: mod\n",programCounter);
+  }else if(zeile==RDINT){
+    printf("%03d: rdint\n",programCounter);
+  }else if(zeile==WRINT){
+    printf("%03d: wrint\n",programCounter);
+  }else if(zeile==ASF){
+    printf("%03d: asf %7d\n",programCounter,(SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==RSF){
+    printf("%03d: rsf\n",programCounter);
+  }else if(zeile==PUSHL){
+    printf("%03d: pushl %5d\n",programCounter,(SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==POPL){
+    printf("%03d: popl %6d\n",programCounter,(SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==EQ){
+    printf("%03d: eq \n",programCounter);
+  }else if(zeile==NE){
+    printf("%03d: ne \n",programCounter);
+  }else if(zeile==LT){
+    printf("%03d: lt \n",programCounter);
+  }else if(zeile==LE){
+    printf("%03d: le \n",programCounter);
+  }else if(zeile==GT){
+    printf("%03d: gt \n",programCounter);
+  }else if(zeile==GE){
+    printf("%03d: ge \n",programCounter);
+  }else if(zeile==JMP){
+    printf("%03d: jmp %2d\n",programCounter,(SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==BRF){
+    printf("%03d: brf %2d\n",programCounter,(SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==BRT){
+    printf("%03d: brt %2d\n",programCounter,(SIGN_EXTEND(code[programCounter]&0x00FFFFFF)));
+  }else if(zeile==PO){
+    printf("%03d: po\n",programCounter);
   }
-  programCounter = 0;
 }
 
-/*code1 und code2 ausfuehren*/
-void program(unsigned int *code,int size){
+/* ausfuehren der Instruktionen */
+void program(unsigned int *code){
   int instruction, n1, n2, eingeleseneZahl;
+  instruction=(code[programCounter]&0xFF000000)>>24;
 
-  printf("Ninja Virtual Machine started\n");
+  if(instruction==HALT){
+    /*break;*/
+  }else if(instruction==PUSHC){ /*schreiben in stack*/
+    push(code[programCounter]);
+  }else if(instruction==ADD){
+    n1=pop();
+    n2=pop();
+    push(n1+n2);
+  }else if(instruction==SUB){
+    n1=pop();
+    n2=pop();
+    push(n1-n2);
+  }else if(instruction==MUL){
+    n1=pop();
+    n2=pop();
+    push(n1*n2);
+  }else if(instruction==DIV){
+    n1=pop();
+    n2=pop();
+    
+    if(n2!=0){
+      push(n1/n2);
+    }else{
 
-  printProgram(code, size);
-
-  /*geht jede Instruktion der Instruktionstabelle durch*/
-  for(programCounter=0;programCounter<size;programCounter++){
-    instruction=(code[programCounter]&0xFF000000)>>24;
-
-    if(instruction==HALT){
-      break;
-    }else if(instruction==PUSHC){ /*schreiben in stack*/
-      push(code[programCounter]);
-    }else if(instruction==ADD){
-      n1=pop();
-      n2=pop();
-      push(n2+n1);
-    }else if(instruction==SUB){
-      n1=pop();
-      n2=pop();
-      push(n1-n2);
-    }else if(instruction==MUL){
-      n1=pop();
-      n2=pop();
-      push(n2*n1);
-    }else if(instruction==DIV){
-      n1=pop();
-      n2=pop();
-      
-      if(n2!=0){
-        push(n2/n1);
-      }
-    }else if(instruction==MOD){
-      n1=pop();
-      n2=pop();
-      push(n1%n2);
-    }else if(instruction==RDINT){
-        /* liest Zahl auf der konsole ein */
-      scanf("%d", &eingeleseneZahl);
-      push(eingeleseneZahl);
-    }else if(instruction==WRINT){
-      printf("%d\n",SIGN_EXTEND(IMMEDIATE(stack[stackPointer-1])));
-    }else if(instruction==ASF){ /*stack frame anlegen, mit groesse n*/
-      push(framePointer);/*position des letzten frames wird gemerkt*/
-      push(IMMEDIATE(code[programCounter]));
-      framePointer=stackPointer;
-      stackPointer=stackPointer+(IMMEDIATE(code[programCounter]));
-    }else if(instruction==RSF){ /*benutzen stack frame entfernen*/
-      stackPointer=framePointer;
-      pop();
-      framePointer=pop(); /*position des letzten frames wird zurueckgeschrieben*/
-    }else if(instruction==PUSHL){ /*variable von frame wird in stack geschrieben*/
-      n2=popFrame(code[programCounter]);
-      push(n2);
-    }else if(instruction==POPL){ /*wert vom stack wird in frame geschrieben*/
-      n1=pop();
-      pushFrame(n1,code[programCounter]);
-    }else if((instruction==EQ) ||
-             (instruction==NE) ||
-             (instruction==LT) ||
-             (instruction==LE) ||
-             (instruction==GT) ||
-             (instruction==GE)){
-
-      n1=pop();
-      n2=pop();
-      /*printf("%d \n", compare(n1, n2, instruction));*/
-      push(compare(n1, n2, instruction));
-    }else if(instruction==JMP){
-      programCounter = IMMEDIATE(code[programCounter]); /* -1 wegen for-schleifen ++ */
-      /*printf("%d\n", code[programCounter]);*/
-    }else if(instruction==BRF){
-      n1=pop();
-      if(n1 == 0){
-	programCounter = IMMEDIATE(code[programCounter]); /* -1 wegen for-schleifen ++ */
-      }else if(n1 == 1){
-	/* nix */	
-      }else{
-	printf("Error in BRF. Stack element is neither 0 nor 1.");
-	exit(-99);	
-      }
-    }else if(instruction==BRT){
-	  n1=pop();
-	 /*printf("%d\n", n1);*/
-	  if(n1 == 1){
-		programCounter = IMMEDIATE(code[programCounter]); /* -1 wegen for-schleifen ++ */
-	  }else if(n1 == 0){
-	    /* nix */	
-	  }else{
-		printf("Error in BRT. Stack element is neither 0 nor 1.");
-		exit(-99);	
-	  }
-    }else if(instruction==PO){
-      printf("Stackpointer: %d\nFramepointer: %d\n",stackPointer,framePointer);
     }
+  }else if(instruction==MOD){
+    n1=pop();
+    n2=pop();
+    push(n1%n2);
+  }else if(instruction==RDINT){
+    /* liest Zahl auf der konsole ein */
+    scanf("%d", &eingeleseneZahl);
+    push(eingeleseneZahl);
+  }else if(instruction==WRINT){
+    printf("%d\n",SIGN_EXTEND(IMMEDIATE(stack[stackPointer-1])));
+  }else if(instruction==ASF){ /*stack frame anlegen, mit groesse n*/
+    push(framePointer);/*position des letzten frames wird gemerkt*/
+    push(IMMEDIATE(code[programCounter]));
+    framePointer=stackPointer;
+    stackPointer=stackPointer+(IMMEDIATE(code[programCounter]));
+  }else if(instruction==RSF){ /*benutzen stack frame entfernen*/
+    stackPointer=framePointer;
+    pop();
+    framePointer=pop(); /*position des letzten frames wird zurueckgeschrieben*/
+  }else if(instruction==PUSHL){ /*variable von frame wird in stack geschrieben*/
+    n2=popFrame(code[programCounter]);
+    push(n2);
+  }else if(instruction==POPL){ /*wert vom stack wird in frame geschrieben*/
+    n1=pop();
+    pushFrame(n1,code[programCounter]);
+  }else if((instruction==EQ) ||
+           (instruction==NE) ||
+           (instruction==LT) ||
+           (instruction==LE) ||
+           (instruction==GT) ||
+           (instruction==GE)){
+    n1=pop();
+    n2=pop();
+    /*printf("%d \n", compare(n1, n2, instruction));*/
+    push(compare(n1, n2, instruction));
+  }else if(instruction==JMP){
+    programCounter = IMMEDIATE(code[programCounter]); /* -1 wegen for-schleifen ++ */
+    /*printf("%d\n", code[programCounter]);*/
+  }else if(instruction==BRF){
+    n1=pop();
+    if(n1 == 0){
+    programCounter = IMMEDIATE(code[programCounter]); /* -1 wegen for-schleifen ++ */
+    }else if(n1 == 1){
+      /* nix */	
+    }else{
+      printf("Error in BRF. Stack element is neither 0 nor 1.");
+      exit(-99);	
+    }
+  }else if(instruction==BRT){
+    n1=pop();
+    /*printf("%d\n", n1);*/
+    if(n1 == 1){
+      programCounter = IMMEDIATE(code[programCounter]); /* -1 wegen for-schleifen ++ */
+    }else if(n1 == 0){
+      /* nix */	
+    }else{
+      printf("Error in BRT. Stack element is neither 0 nor 1.");
+      exit(-99);	
+    }
+  }else if(instruction==PO){
+    printf("Stackpointer: %d\nFramepointer: %d\n",stackPointer,framePointer);
   }
-
-  stackPointer=0;
-  framePointer=0;
-  printf("Ninja Virtual Machine stopped\n");
 }
+
 
 /* Vergleicht zwei Zahlen */
 int compare(int n1, int n2, int instruction){
